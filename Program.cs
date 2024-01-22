3a04ecfdaa23058a7d12b9b8eb41db08930e6af1
timoteifrincuu <146390788+timoteifrincuu@users.noreply.github.com>
2024-01-22 10:06:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace examen2
+{
+
+    internal class Program
+    {
+        static void Main()
+        {
+            string customerName = Console.ReadLine();
+            DateTime orderDate = DateTime.Parse(Console.ReadLine());
+            decimal ordereTotal = decimal.Parse(Console.ReadLine());
+        
+            Order order = new Order(
+                customerName = customerName,
+                Date = orderDate,
+                Total = orderTotal    
+                );  
+        }
+    }
+}
