2d906808d8855ee47410d5b0c6289e6779849962
timoteifrincuu <146390788+timoteifrincuu@users.noreply.github.com>
2024-01-22 09:51:38 +0200

    Add files via upload

---
+++ paymentsystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public abstract class PaymentSystem
+{
+    public abstract void ProcessPayment(decimal amount);
+}
+
+public class CardPayment : PaymentSystem
+{
+    public override void ProcessPayment(decimal amount)
+    {
+        Console.WriteLine("Plata online cu cardul");
+    }
+}
+
+public class PayPalPayment : PaymentSystem
+{
+    public override void ProcessPayment(decimal amount)
+    {
+        Console.WriteLine("Plata prin PayPal");
+    }
+}
+
+public class CryptoPayment : PaymentSystem
+{
+    public override void ProcessPayment(decimal amount)
+    {
+        Console.WriteLine("Plata cu wallet crypto");
+    }
+}
